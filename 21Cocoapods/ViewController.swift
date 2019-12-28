e6bdfcf29714423a9ffea3f24f0c46fb38812c7b
Denis Vitios <denvitios@gmail.com>
2019-12-28 13:35:45 +0200

    Initial Commit

---
+++ 21Cocoapods/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  21Cocoapods
+//
+//  Created by Denis on 28.12.2019.
+//  Copyright © 2019 Denis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
