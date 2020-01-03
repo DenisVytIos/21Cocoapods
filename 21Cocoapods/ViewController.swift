7ed434b375b53b0a945cc017fa4af80283fe2bc4
Denis Vitios <denvitios@gmail.com>
2020-01-03 08:11:49 +0200

    finish progect

--- 21Cocoapods/ViewController.swift
+++ 21Cocoapods/ViewController.swift
@@ -7,14 +7,45 @@
 //
 
 import UIKit
+import Jelly
+import LTMorphingLabel
 
 class ViewController: UIViewController {
+    
+    var animator: Animator?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        let label = LTMorphingLabel(frame: CGRect(x: 100, y: 100, width: 200, height: 200))
+        label.text = "Hello brather"
+        view.addSubview(label)
+        NSLayoutConstraint.activate([view.centerXAnchor.constraint(equalTo: label.centerXAnchor, constant: 0), view.centerYAnchor.constraint(equalTo: label.centerYAnchor, constant: 100)])
+        view.layoutIfNeeded()
+        label.morphingEffect = .burn
+        label.morphingEnabled = true
+        label.morphingDuration = 3
+        label.morphingCharacterDelay = 0.2
+        label.start()
+        
+        
+        let viewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(identifier: "VC")
+        let interaction = InteractionConfiguration(presentingViewController: self, completionThreshold: 0.5, dragMode: .edge, mode: .dismiss)
+        let presentation = SlidePresentation(direction: .right, interactionConfiguration: interaction)
+        animator = Animator(presentation: presentation)
+        animator?.prepare(presentedViewController: viewController)
     }
 
-
+    @IBAction func moveToVC() {
+        
+        let viewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(identifier: "VC")
+        
+        let slidePresentation = SlidePresentation(direction: .top)
+        let animator = Animator(presentation: slidePresentation)
+        animator.prepare(presentedViewController: viewController)
+        present(viewController, animated: true, completion: nil)
+    }
+    
 }
 
---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target '21Cocoapods' do
+  # Comment the next line if you don't want to use dynamic frameworks
+
+  pod 'Jelly', '~> 2.2.2'
+  pod 'LTMorphingLabel'
+  use_frameworks!
+
+  # Pods for 21Cocoapods
+
+end
